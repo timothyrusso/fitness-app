f0d256838a5727a47aeab5fbadab0639c5214351
timothyrusso <russotimothy@live.it>
2023-11-03 17:59:00 +0100

    feat: add linear gradient expo dependency

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #Fitness App
+
+Docs: [Code Structure](https://kxie0124.medium.com/design-react-native-app-with-mvp-model-view-presenter-architecture-d451d35d6cac)
--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -7,6 +7,7 @@ import {
   Button,
   KeyboardAvoidingView,
 } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import React, { useState } from 'react';
 import { FIREBASE_AUTH } from '../../FirebaseConfig';
 import {
@@ -52,49 +53,56 @@ const Login = () => {
 
   return (
     <View style={styles.container}>
-      <KeyboardAvoidingView behavior="padding">
-        <TextInput
-          value={email}
-          style={styles.input}
-          placeholder="Email"
-          autoCapitalize="none"
-          onChangeText={(text) => setEmail(text)}
-        ></TextInput>
-        <TextInput
-          secureTextEntry={true}
-          value={password}
-          style={styles.input}
-          placeholder="Password"
-          autoCapitalize="none"
-          onChangeText={(text) => setPassword(text)}
-        ></TextInput>
-        {loading ? (
-          <ActivityIndicator size="large" color="#0000ff" />
-        ) : (
-          <>
-            <CustomButton
-              title="Sign In"
-              onPress={signIn}
-              buttonStyle={{
-                borderRadius: 20,
-                marginVertical: 5,
-                width: '70%',
-                alignSelf: 'center',
-              }}
-            />
-            <CustomButton
-              title="Sign Up"
-              onPress={signUp}
-              buttonStyle={{
-                borderRadius: 20,
-                marginVertical: 5,
-                width: '70%',
-                alignSelf: 'center',
-              }}
-            />
-          </>
-        )}
-      </KeyboardAvoidingView>
+      <LinearGradient
+        colors={['#051937', '#004d7a', '#008793', '#00bf72', '#a8eb12']}
+        style={styles.container}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+      >
+        <KeyboardAvoidingView behavior="padding">
+          <TextInput
+            value={email}
+            style={styles.input}
+            placeholder="Email"
+            autoCapitalize="none"
+            onChangeText={(text) => setEmail(text)}
+          ></TextInput>
+          <TextInput
+            secureTextEntry={true}
+            value={password}
+            style={styles.input}
+            placeholder="Password"
+            autoCapitalize="none"
+            onChangeText={(text) => setPassword(text)}
+          ></TextInput>
+          {loading ? (
+            <ActivityIndicator size="large" color="#0000ff" />
+          ) : (
+            <>
+              <CustomButton
+                title="Sign In"
+                onPress={signIn}
+                buttonStyle={{
+                  borderRadius: 20,
+                  marginVertical: 5,
+                  width: '70%',
+                  alignSelf: 'center',
+                }}
+              />
+              <CustomButton
+                title="Sign Up"
+                onPress={signUp}
+                buttonStyle={{
+                  borderRadius: 20,
+                  marginVertical: 5,
+                  width: '70%',
+                  alignSelf: 'center',
+                }}
+              />
+            </>
+          )}
+        </KeyboardAvoidingView>
+      </LinearGradient>
     </View>
   );
 };
@@ -103,14 +111,17 @@ export default Login;
 
 export const styles = StyleSheet.create({
   container: {
-    marginHorizontal: 20,
     flex: 1,
     justifyContent: 'center',
   },
+  gradientContainer: {
+    flex: 1,
+  },
   input: {
+    marginHorizontal: 20,
     marginVertical: 10,
     height: 50,
-    borderWidth: 1.5,
+    borderWidth: 1,
     borderRadius: 28,
     padding: 20,
     backgroundColor: '#fff',
