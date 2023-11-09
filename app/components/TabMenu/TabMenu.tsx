eebb64ef7481368e7d3f816a1e5d8d5710f4a278
Timothy Russo <russotimothy@live.it>
2023-11-09 09:34:59 +0100

    feat: refactor the tab menu component

--- app/components/TabMenu/TabMenu.tsx
+++ app/components/TabMenu/TabMenu.tsx
@@ -8,62 +8,61 @@ import { BlurView } from 'expo-blur';
 
 const Tab = createBottomTabNavigator();
 
+const tabsConfig = [
+  {
+    name: 'Main page',
+    component: ExamplePage,
+    icon: 'home',
+  },
+  {
+    name: 'Notifications',
+    component: ExamplePageSecond,
+    icon: 'search',
+  },
+  {
+    name: 'Exercises',
+    component: ExamplePage,
+    icon: 'link',
+  },
+  {
+    name: 'Profile',
+    component: ExamplePageSecond,
+    icon: 'body',
+  },
+];
+
+console.log(tabsConfig);
+
 const TabMenu = () => {
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarStyle: styles.container,
+        tabBarStyle: { ...styles.container },
         tabBarBackground: () => (
           <BlurView tint="dark" intensity={40} style={{ flex: 1 }} />
         ),
         tabBarInactiveTintColor: 'black',
         tabBarActiveTintColor: 'white',
         headerShown: false,
+        tabBarShowLabel: false,
       }}
     >
-      <Tab.Screen
-        name="Main page"
-        component={ExamplePage}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="home" color={color} size={20} />
-          ),
-          tabBarShowLabel: false,
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={ExamplePageSecond}
-        options={{
-          tabBarLabel: 'Updates',
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="search" color={color} size={20} />
-          ),
-          tabBarShowLabel: false,
-        }}
-      />
-      <Tab.Screen
-        name="Exercises"
-        component={ExamplePage}
-        options={{
-          tabBarLabel: 'Exercises',
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="link" color={color} size={20} />
-          ),
-          tabBarShowLabel: false,
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ExamplePage}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="body" color={color} size={20} />
-          ),
-          tabBarShowLabel: false,
-        }}
-      />
+      {tabsConfig.map((tab) => (
+        <Tab.Screen
+          key={tab.name}
+          name={tab.name}
+          component={tab.component}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Ionicons
+                name={tab.icon as keyof typeof Ionicons.glyphMap}
+                color={color}
+                size={20}
+              />
+            ),
+          }}
+        />
+      ))}
     </Tab.Navigator>
   );
 };
