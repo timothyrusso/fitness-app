86736a9be989b129cb8e68ff2313c13506060b2a
Timothy Russo <russotimothy@live.it>
2023-11-10 08:55:52 +0100

    feat: profile page structure

--- app/components/Widget/Widget.tsx
+++ app/components/Widget/Widget.tsx
@@ -1,9 +1,13 @@
-import { View } from 'react-native';
+import { StyleProp, View, ViewStyle } from 'react-native';
 import React from 'react';
 import styles from './Widget.style';
 
-const Widget = () => {
-  return <View style={styles.container}></View>;
+interface WidgetProps {
+  customStyle?: StyleProp<ViewStyle>;
+}
+
+const Widget = ({ customStyle }: WidgetProps) => {
+  return <View style={[styles.container, customStyle]}></View>;
 };
 
 export default Widget;
--- app/screens/Profile.tsx
+++ app/screens/Profile.tsx
@@ -1,10 +1,10 @@
-import { Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import React from 'react';
-import { styles } from './Login';
 import IconButton from '../components/IconButton/IconButton';
 import { FIREBASE_AUTH } from '../../FirebaseConfig';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { NavigationProp } from '@react-navigation/native';
+import Widget from '../components/Widget/Widget';
 
 interface ProfileProps {
   navigation: NavigationProp<any, any>;
@@ -13,21 +13,70 @@ interface ProfileProps {
 const ProfilePage = ({ navigation }: ProfileProps) => {
   return (
     <View style={styles.container}>
-      <Text>Profile Page</Text>
       <IconButton
         onPress={() => navigation.navigate('Example Page')}
         icon={<Ionicons name="settings-outline" color={'black'} size={20} />}
-        buttonColor="trasparent"
-        buttonStyle={{ position: 'absolute', top: 20, left: 20, zIndex: 1 }}
+        buttonColor="red"
+        buttonStyle={{
+          position: 'absolute',
+          top: 20,
+          left: 20,
+          zIndex: 1,
+          borderRadius: 100,
+        }}
       />
       <IconButton
         onPress={() => FIREBASE_AUTH.signOut()}
         icon={<Ionicons name="exit-outline" color={'black'} size={20} />}
-        buttonColor="trasparent"
-        buttonStyle={{ position: 'absolute', top: 20, right: 20, zIndex: 1 }}
+        buttonColor="red"
+        buttonStyle={{
+          position: 'absolute',
+          top: 20,
+          right: 20,
+          zIndex: 1,
+          borderRadius: 100,
+        }}
+      />
+      <View
+        style={{
+          alignItems: 'flex-start',
+          width: '100%',
+          height: '30%',
+          backgroundColor: 'blue',
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+        }}
+      ></View>
+      <View
+        style={{
+          width: 130,
+          height: 130,
+          borderRadius: 100,
+          backgroundColor: 'green',
+          position: 'absolute',
+          marginTop: '35%',
+          alignSelf: 'center',
+        }}
+      ></View>
+      <Widget
+        customStyle={{
+          marginTop: '25%',
+          marginHorizontal: 20,
+        }}
+      />
+      <Widget
+        customStyle={{
+          marginHorizontal: 20,
+        }}
       />
     </View>
   );
 };
 
 export default ProfilePage;
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
